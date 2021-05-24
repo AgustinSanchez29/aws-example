ea26de09c48cd34e8be938f1958249e6ce863b47
AgustinSanchez29 <agustin.s2966@gmail.com>
2021-05-23 22:58:19 -0500

    port updated

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get('/',(req,res)=>{
     res.send('<h1>Hi, now on express and html</h1>')
 })
 
-app.listen(3000,(console.log(`listen on port 3000`)));
+app.listen(8080,(console.log(`listen on port 8080`)));
