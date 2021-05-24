468e6bd09799c385160355770b3fc2114ab6c374
AgustinSanchez29 <agustin.s2966@gmail.com>
2021-05-23 22:55:49 -0500

    texto modificado

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('Hi')
+    res.send('<h1>Hi, now on express and html</h1>')
 })
 
 app.listen(3000,(console.log(`listen on port 3000`)));
